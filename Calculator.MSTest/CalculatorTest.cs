91876e456b64b7886b61e389f64ba76e5aa67ae6
Miguel Peraza R <miguel.peraza.rosado@accenture.com>
2019-12-03 11:20:32 -0600

    Add project files.

---
+++ Calculator.MSTest/CalculatorTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.MSTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void GivenTwoValues_WhenPerformingAddOperation_ThenShouldReturnTheSum()
+        {
+            //Arrenge
+            double expectet = 2;
+            double x = 1;
+            double y = 1;
+
+            //Act
+            var result = Calculator.Add(x, y);
+
+            //Assert
+            Assert.AreEqual(expectet, result);
+        }
+
+        [TestMethod]
+        public void GivenTwoValues_WhenPerformingSubstractOperation_ThenShouldReturnTheDifference()
+        {
+            //Arrenge
+            double expectet = 0;
+            double x = 1;
+            double y = 1;
+
+            //Act
+            var result = Calculator.Substract(x, y);
+
+            //Assert
+            Assert.AreEqual(expectet, result);
+        }
+
+        [TestMethod]
+        public void GivenTwoValues_WhenPerformingMultiplyOperation_ThenShouldReturnTheNewValue()
+        {
+            //Arrenge
+            double expectet = 3;
+            double x = 1;
+            double y = 3;
+
+            //Act
+            var result = Calculator.Multipliy(x, y);
+
+            //Assert
+            Assert.AreEqual(expectet, result);
+        }
+
+        [TestMethod]
+        public void GivenTwoValues_WhenPerformingDivisionOperation_ThenShouldReturnTheNewValue()
+        {
+            //Arrenge
+            double expectet = 2;
+            double x = 4;
+            double y = 2;
+
+            //Act
+            var result = Calculator.Divide(x, y);
+
+            //Assert
+            Assert.AreEqual(expectet, result);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multipliy(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
